a390e769d5eadf5553cb01af377f3aa5f952054c
jacobdekeizer <jacobdekeizer@gmail.com>
2021-05-16 20:18:12 +0200

    Improve select

--- src/components/form/Select.vue
+++ src/components/form/Select.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="relative">
-        <select class="block appearance-none w-full bg-blue-gray border-2 border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+    <div class="relative inline-block w-full">
+        <select class="w-full px-3 py-2 border-2 border-gray-200 text-gray-700 rounded-md appearance-none bg-blue-gray focus:bg-white focus:outline-none focus:border-cyan-400 transition-colors "
                 :id="id"
                 :value="value"
                 @input="onValueChanged($event.target.value)">
@@ -8,9 +8,12 @@
                 {{ option.text }}
             </option>
         </select>
-        <div class="absolute flex inset-y-0 items-center px-3 right-0 text-gray-700 bg-gray-300 rounded-r pointer-events-none">
-            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+        <div class='absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none text-gray-500'>
+            <svg class='w-5 h-5 fill-current' viewBox='0 0 20 20'>
+            <path
+                d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0
+                111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
+                clipRule='evenodd' fillRule='evenodd' />
             </svg>
         </div>
     </div>
