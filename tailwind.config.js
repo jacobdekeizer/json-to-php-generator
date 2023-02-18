b8dd2adbd3a59553833ebcff4783258e077862a8
jacobdekeizer <jacobdekeizer@gmail.com>
2023-02-18 12:49:12 +0100

    Upgrade tailwindcss to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   darkMode: 'class',
-  purge: [
+  content: [
     './public/**/*.html',
     './src/**/*.vue',
     './src/**/*.scss',
